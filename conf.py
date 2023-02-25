6c121772889c4426c39f5f49ae94ec1f01124c05
Mohammad Mehdi Begmaz <94954298+MohammadMehdi81@users.noreply.github.com>
2023-02-25 21:47:24 +0330

    Add files via upload

--- conf.py
+++ conf.py
@@ -1,118 +1,150 @@
-# -*- coding: utf-8 -*-
+# Configuration file for the Sphinx documentation builder.
+#
+# Full list of options can be found in the Sphinx documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# pip install furo
-
-import sys
 import os
-import re
+import sys
+from typing import Any, Dict
 
-# Prefer to use the version of the theme in this repo
-# and not the installed version of the theme.
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.append(os.path.abspath('./demo/'))
+# add the demo python code to the path, so that it can be used to demonstrate
+# source links
+sys.path.append(os.path.abspath("./kitchen-sink/demo_py"))
 
-from sphinx_rtd_theme import __version__ as theme_version
-from sphinx_rtd_theme import __version_full__ as theme_version_full
-from sphinx.locale import _
+# -- Project information -----------------------------------------------------
+#
 
-project = u'Read the Docs Sphinx Theme'
-slug = re.sub(r'\W+', '-', project.lower())
-version = theme_version
-release = theme_version_full
-author = u'Dave Snider, Read the Docs, Inc. & contributors'
-copyright = author
-language = 'en'
+project = "furo"
+copyright = "2020, Pradyun Gedam"
+author = "Pradyun Gedam"
+
+# -- General configuration ---------------------------------------------------
+#
 
 extensions = [
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx_rtd_theme',
+    # Sphinx's own extensions
+    "sphinx.ext.autodoc",
+    "sphinx.ext.extlinks",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    # Our custom extension, only meant for Furo's own documentation.
+    "furo.sphinxext",
+    # External stuff
+    "myst_parser",
+    "sphinx_copybutton",
+    "sphinx_design",
+    "sphinx_inline_tabs",
 ]
+templates_path = ["_templates"]
 
-templates_path = ['_templates']
-source_suffix = '.rst'
-exclude_patterns = []
-locale_dirs = ['locale/']
-gettext_compact = False
+# -- Options for extlinks ----------------------------------------------------
+#
 
-master_doc = 'index'
-suppress_warnings = ['image.nonlocal_uri']
-pygments_style = 'default'
+extlinks = {
+    "pypi": ("https://pypi.org/project/%s/", "%s"),
+}
 
-if sys.version_info < (3, 0):
-    tags.add("python2")
-else:
-    tags.add("python3")
+# -- Options for intersphinx -------------------------------------------------
+#
 
 intersphinx_mapping = {
-    'rtd': ('https://docs.readthedocs.io/en/stable/', None),
-    'rtd-dev': ('https://dev.readthedocs.io/en/stable/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "python": ("https://docs.python.org/3", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master", None),
 }
 
-html_theme = 'furo'
-html_theme_options = {
-    'logo_only': True,
-    'navigation_depth': 5,
+# -- Options for TODOs -------------------------------------------------------
+#
+
+todo_include_todos = True
+
+# -- Options for Markdown files ----------------------------------------------
+#
+
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+]
+myst_heading_anchors = 3
+
+# -- Options for HTML output -------------------------------------------------
+#
+
+html_theme = "furo"
+html_title = "Furo"
+language = "en"
+
+html_static_path = ["_static"]
+html_css_files = ["pied-piper-admonition.css"]
+
+html_theme_options: Dict[str, Any] = {
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/pradyunsg/furo",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
+    "source_repository": "https://github.com/pradyunsg/furo/",
+    "source_branch": "main",
+    "source_directory": "docs/",
 }
-html_context = {}
 
-if not 'READTHEDOCS' in os.environ:
-    html_static_path = ['_static/']
-    html_js_files = ['debug.js']
-
-    # Add fake versions for local QA of the menu
-    html_context['test_versions'] = list(map(
-        lambda x: str(x / 10),
-        range(1, 100)
-    ))
-
-html_logo = "demo/static/logo-wordmark-light.svg"
-html_show_sourcelink = True
-html_favicon = "demo/static/favicon.ico"
-
-htmlhelp_basename = slug
-
-
-latex_documents = [
-  ('index', '{0}.tex'.format(slug), project, author, 'manual'),
-]
-
-man_pages = [
-    ('index', slug, project, [author], 1)
-]
-
-texinfo_documents = [
-  ('index', slug, project, author, slug, project, 'Miscellaneous'),
-]
-
-
-# Extensions to theme docs
-def setup(app):
-    from sphinx.domains.python import PyField
-    from sphinx.util.docfields import Field
-
-    app.add_object_type(
-        'confval',
-        'confval',
-        objname='configuration value',
-        indextemplate='pair: %s; configuration value',
-        doc_field_types=[
-            PyField(
-                'type',
-                label=_('Type'),
-                has_arg=False,
-                names=('type',),
-                bodyrolename='class'
-            ),
-            Field(
-                'default',
-                label=_('Default'),
-                has_arg=False,
-                names=('default',),
-            ),
-        ]
+if "READTHEDOCS" in os.environ:
+    html_theme_options["announcement"] = (
+        "This documentation is hosted on Read the Docs only for testing. Please use "
+        "<a href='https://pradyunsg.me/furo/'>the main documentation</a> instead."
     )
+
+# -- Options for theme development -------------------------------------------
+# Make sure these are all set to the default values.
+
+html_js_files = []
+html_context: Dict[str, Any] = {}
+# html_show_sphinx = False
+# html_show_copyright = False
+# html_last_updated_fmt = ""
+
+RTD_TESTING = False
+if RTD_TESTING or "FURO_RTD_TESTING" in os.environ:
+    del html_theme_options["footer_icons"]
+
+    html_css_files += [
+        "https://assets.readthedocs.org/static/css/readthedocs-doc-embed.css",
+        "https://assets.readthedocs.org/static/css/badge_only.css",
+    ]
+    html_js_files += [
+        "readthedocs-dummy.js",
+        "https://assets.readthedocs.org/static/javascript/readthedocs-doc-embed.js",
+    ]
+    html_context["READTHEDOCS"] = True
+    html_context["current_version"] = "latest"
+    html_context["conf_py_path"] = "/docs/"
+    html_context["display_github"] = True
+    html_context["github_user"] = "pradyunsg"
+    html_context["github_repo"] = "furo"
+    html_context["github_version"] = "main"
+    html_context["slug"] = "furo"
+
+FONT_AWESOME_TESTING = False
+if FONT_AWESOME_TESTING:
+    html_css_files += [
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/fontawesome.min.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/solid.min.css",
+        "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/brands.min.css",
+    ]
+
+    html_theme_options["footer_icons"] = [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/pradyunsg/furo",
+            "html": "",
+            "class": "fa-solid fa-github fa-2x",
+        },
+    ]
